d7e87eaf7a33066d4603058912f38674ce7fcd04
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-02-27 14:44:59 +0700

    feat: add useCallback in getItems function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.scss";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import List from "./List";
 
 export default function Home() {
   const [number, setNumber] = useState(1);
   const [darkTheme, setDarkTheme] = useState(false);
 
-  const getItems = () => {
+  const getItems = useCallback(() => {
     return [number, number + 1, number + 2];
-  };
+  }, [number]);
 
   return (
     <>
